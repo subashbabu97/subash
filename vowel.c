42c36ebc0f2e898107aa5bc659d5f0069c2a4556
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-03-07 21:27:07 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+char=('a','e','i','o','u');
+if("char=a||A,char=e||E,char=i||I,char=o||O,char=u||U")
+printf("%c is a vowel");
+else
+printf("%c is not a vowel");
