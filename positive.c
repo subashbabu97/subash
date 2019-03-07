9ff58dfe83e7baa1f870b33b37464f3626a791fd
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-03-07 21:03:12 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int=num;
+printf("%d is a num);
+scanf("%d,&n);
+if(num<0)
+printf("%d is negative");
+ifelse(num>0)
+print("%d is positive");
+else
+print("%d is zero);
